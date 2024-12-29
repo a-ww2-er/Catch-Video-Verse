82abe267d35d498a47ec4c9e94c435b906f30cb8
Zack Agba <zackagba7@gmail.com>
2024-12-29 17:59:51 +0100

    CLient folder & Server

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+       <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600">
+        Welcome to Next.js with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
