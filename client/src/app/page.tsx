e234ec75eae310c1827133c6817b28a94a5a1e68
Zack Agba <zackagba7@gmail.com>
2025-01-04 17:47:46 +0100

    Albert Sans added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,9 @@
-
+import '@/styles/fonts.css';
+import Navbar from "./Components/Navbar";
 export default function Home() {
   return (
-       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-600">
-        Welcome to Next.js with Tailwind CSS!
-      </h1>
-    </div>
+  <>
+  <Navbar />
+  </>
   );
 }
